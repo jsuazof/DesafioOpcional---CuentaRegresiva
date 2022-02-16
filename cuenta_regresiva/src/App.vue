377e47b88b6caae06c13848b8508b4f713f2734b
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-16 18:20:06 -0300

    primer commit

---
+++ cuenta_regresiva/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    {{ timerCount }}
+  </div>
+    
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                timerCount: 30
+            }
+        },
+        watch: {
+            timerCount: {
+                handler(value) {
+                    if (value > 0) {
+                        setTimeout(() => {
+                            this.timerCount--;
+                        }, 1000);
+                    }
+                },
+                immediate: true // This ensures the watcher is triggered upon creation
+            }
+        }
+    }
+
+</script>
